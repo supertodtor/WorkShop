4f085746dd496213c5d661056e32335ba7572412
todtor <ta_tor67@hotmail.com>
2017-07-28 16:04:15 +0700

    addListView

--- ListView/app/src/main/java/com/mycom/coe/listview/MainActivity.java
+++ ListView/app/src/main/java/com/mycom/coe/listview/MainActivity.java
@@ -2,12 +2,56 @@ package com.mycom.coe.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[]{
+                "Item 1: Android",
+                "Item 2: iPhone ",
+                "Item 3: Windows Phone",
+                "Item 4: Custom Phone"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written (related to layout)
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+
+        //position starting from 0
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+ position+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+
+        });
+
+
+
     }
 }
