bb0ca4b99f0a32427245c65b29c22da861a34c64
todtor <ta_tor67@hotmail.com>
2017-07-30 13:19:47 +0700

    addNontification

--- MyFragment2/app/src/main/java/com/mycom/coe/myfragment/MainActivity.java
+++ MyFragment2/app/src/main/java/com/mycom/coe/myfragment/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mycom.coe.myfragment;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,21 +23,62 @@ public class MainActivity extends AppCompatActivity {
     private Button btnTwo;
     private Button btnThree;
     private Button btnBack;
+    private int mNotificationId;
+    private Button btnNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         startBlankFragment();
 
         reMoveFragment();
 
-//        startJavaFragment();
-
+        startJavaFragment();
 
         btnAdd();
 
         btnBack();
+
+        notification();
+
+    }
+
+    private void notification() {
+        btnNotification = (Button) findViewById(R.id.btnNotification);
+
+        btnNotification.setOnClickListener(new View.OnClickListener() {
+            public int NOTIFICATION_ID;
+
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder mBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.this)
+                                .setSmallIcon(R.drawable.ic_stat_name)
+                                .setContentTitle("My notification")
+                                .setContentText("Open Google website!");
+
+                Intent resultIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://www.google.com/"));
+
+                //  Define notification action
+                PendingIntent resultPendingIntent =
+                        PendingIntent.getActivity(
+                                MainActivity.this,   // context
+                                0,      // requestCode (defined PendingIntent id)
+                                resultIntent,
+                                // if a previous PendingIntent already exists,
+                                // then the current one will update it with the latest intent
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(resultPendingIntent);
+
+                NotificationManager mNotifyMgr =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                // Builds the notification and issues it.
+                mNotifyMgr.notify(NOTIFICATION_ID, mBuilder.build());
+            }
+        });
     }
 
     private void btnBack() {
